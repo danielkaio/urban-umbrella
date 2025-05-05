7b0768900b753be15e7edf8f154589c4b609951d
daniel <dani.ajala@yahoo.com>
2025-05-05 03:10:05 -0300

    iniciando conexao com db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PessoaModule } from './pessoa.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '172.17.0.2',
+      port: 3306,
+      username: 'root',
+      password: '12345',
+      database: 'cinema',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+
 
 
 @Module({
