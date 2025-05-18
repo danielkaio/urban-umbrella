2bd5badae1df22612953eafd9f3bf1ba4c9f501c
daniel <dani.ajala@yahoo.com>
2025-05-18 16:07:15 -0300

    adicionada config docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,27 @@ import { Module } from '@nestjs/common';
 import { PessoaModule } from './pessoa.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'mysql',
-//       host: '172.17.0.2',
-//       port: 3306,
-//       username: 'root',
-//       password: '12345',
-//       database: 'cinema',
-//       entities: [],
-//       synchronize: true,
-//     }),
-//   ],
-// })
 @Module({
   imports: [PessoaModule],
   controllers: [],
   providers: [],
 })
+
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '172.18.0.2',
+      port: 3306,
+      username: 'root',
+      password: '12345',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+
 export class AppModule {}
